3c66fcf28ebba8c8b10e669f50fae02a632525bd
Suveer-Upasani <suveerupasani@gmail.com>
2025-10-17 23:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
     os.makedirs('data', exist_ok=True)
     
     # Get port from environment variable or use default
-    port = int(os.environ.get('PORT', 5005))
+    port = int(os.environ.get('PORT', 8080))
     
     # Run the app
     app.run(host='0.0.0.0', port=port, debug=False)
